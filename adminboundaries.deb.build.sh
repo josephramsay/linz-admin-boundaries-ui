3b0f7876abd5a11da541469d86a7fbb99b0e0267
Joseph Ramsay <jramsay@linz.govt.nz>
2016-09-07 16:19:51 +1200

    improved deb builder

--- adminboundaries.deb.build.sh
+++ adminboundaries.deb.build.sh
@@ -11,23 +11,55 @@ upstream="$proj_lcase"_$release.orig.tar
 ddate=`date --rfc-2822`
 desc1="Downloads and imports AdminBoundary shapefile data"
 desc2="Internal download and import script for AdminBoundary data as supplied by external agencies."
-pydir="$project/scripts"
-#exculde filled ini
-pybins="download_admin_bdys.py download_admin_bdys.sh"
-#files in this list will be coped from a matching .template file (TODO)
-pyrepl="download_admin_bdys.ini"
-wardir="$project/build/libs"
-warlist="ab.war"
-debdir="debian"
 
-#-----------------------
+#root_dir=`pwd`
+root_dir=~/git
+temp_dir=$root_dir
+temp_dir=/tmp
+
+#code source dir
+src_dir=$root_dir/$project
+#project build dir
+###proj_dir=$root_dir/$proj_temp
+proj_dir=$temp_dir/$project-$release
+
+#code source for python scripts
+python_dir="$src_dir/scripts"
+#code source for web app
+war_dir="$src_dir/build/libs"
+#debian project sub dir where debian files are built
+debian_dir="$proj_dir/debian"
+data_dir="$proj_dir/data"
+#installation dir where package is extracted
+install_dir="/usr/local/share/$project"
+
+war_list="ab.war"
+#exculde filled ini
+script_list="download_admin_bdys.py download_admin_bdys.sh"
+#files in this list will be coped from a matching .template file (TODO)
+conf_replace="download_admin_bdys.ini"
+
+#---------------------------------------------------------------------
+
+if [ $CATALINA_BASE ]
+then
+	cbase=$CATALINA_BASE
+else
+	#cbase=/opt/apache-tomcat/webapps
+	cbase=/var/lib/tomcat/webapps
+	echo "*WARN* Please set CATALINA_BASE, defaulting to $cbase"
+fi
+
+#---------------------------------------------------------------------
+
 function setupdebian {
-	cd $1
-	mkdir $debdir
+	# DEBIAN
+	deb_dir=$1
+	mkdir $deb_dir
 	
 	# CHANGELOG
 	#dch --create -v 1.0-1 --package $proj_lcase
-	cat << EOF > $debdir/changelog
+	cat << EOF > $deb_dir/changelog
 $proj_lcase ($release) UNRELEASED; urgency=low
 
   * Initial release. (Closes: #XXXXXX)
@@ -35,17 +67,20 @@ $proj_lcase ($release) UNRELEASED; urgency=low
  -- $DEBFULLNAME <$DEBEMAIL>  $ddate
 
 EOF
+
 	# RULES
-	cat << EOF > debian/rules 
+	cat << EOF > $deb_dir/rules 
 #!/usr/bin/make -f
 export DH_VERBOSE = 1
 %:
 	dh \$@
 	
+override_dh_usrlocal:
+	
 EOF
 
 	# CONTROL
-	cat << EOF > $debdir/control 
+	cat << EOF > $deb_dir/control 
 Source: $proj_lcase
 Maintainer: $DEBFULLNAME <$DEBEMAIL>
 Section: misc
@@ -63,7 +98,7 @@ Description: $desc1
 EOF
 
 	# COPYRIGHT
-	cat << EOF > $debdir/copyright
+	cat << EOF > $deb_dir/copyright
 Copyright:
 
     Copyright 2011 Crown copyright (c) Land Information New Zealand and the New
@@ -100,93 +135,114 @@ License:
 EOF
 
 	# COMPAT
-	echo 9 > $debdir/compat
+	echo 9 > $deb_dir/compat
 	
 	# INSTALL
-	cat << EOF > $debdir/install 
+	ins_dir=${install_dir#"/"}
+	cat << EOF > $deb_dir/install 
 data/$proj_abv.desktop usr/share/applications
 data/$proj_abv.png usr/share/pixmaps
-scripts/download_admin_bdys.sh usr/bin
-scripts/download_admin_bdys.py usr/bin
-scripts/$pyrepl usr/conf
-libs/ab.war $CATALINA_BASE/webapps
+scripts/download_admin_bdys.sh $ins_dir
+scripts/download_admin_bdys.py $ins_dir
+scripts/$conf_replace $ins_dir
+libs/$war_list ${cbase#"/"}
 EOF
 
 	#-------------------
-	mkdir $debdir/source
+	mkdir $deb_dir/source
 	
 	# SOURCE/FORMAT
-	cat << EOF > $debdir/source/format
+	cat << EOF > $deb_dir/source/format
 3.0 (quilt)
 EOF
 
 	# SOURCE/INC-BIN
-	cat << EOF > $debdir/source/include-binaries
-$debdir/$proj_lcase/usr/share/pixmaps/$proj_abv.png
-$debdir/$proj_lcase/usr/share/doc/$proj_lcase/changelog.gz
+	cat << EOF > $deb_dir/source/include-binaries
+data/$proj_abv.png
+libs/$war_list
 EOF
 
-	cd ..
 }
-#-----------------------
+
+#---------------------------------------------------------------------
 
 
-function appwrapper {
+function setupdesktop {
 	# DESKTOP
-	mkdir $proj_temp/data
-	cp $project/"$proj_abv".png $proj_temp/data/
-	cat << EOF > $proj_temp/data/$proj_abv.desktop
+	dat_dir=$1
+	mkdir $dat_dir
+	cp "$src_dir/$proj_abv.png" $dat_dir
+	cat << EOF > $dat_dir/$proj_abv.desktop
 [Desktop Entry]
 Name=$project
 Comment=Trigger download and import of $project
 Type=Application
 Keywords=$proj_abv
-Exec=/usr/bin/download_admin_bdys.sh
-Terminal=true
+StartupNotify=true
+Exec=$install_dir/download_admin_bdys.py
+Path=$install_dir/
+Terminal=false
 Icon=/usr/share/pixmaps/$proj_abv.png
 Categories=Utility;
 EOF
-
 }
 
+function clean {
+	rm -R $proj_dir
+	rm $temp_dir/$proj_lcase*
+}
+
+#---------------------------------------------------------------------
+
+# ***************
+# *  *********  *
+# *  * START *  *
+# *  *********  *
+# ***************
 
 echo
 echo "*** Debian Package Builder for $project ***"
 echo
 
-cd ~/git/
-pwd
+clean
+
+cd $root_dir
 
 echo "* 1. setup upstream package"
 
-mkdir $proj_temp
-mkdir $proj_temp/scripts
+mkdir $proj_dir
+mkdir $proj_dir/scripts
 
-for p in $pybins
+for p in $script_list
 do
-	cp -R $pydir/$p $proj_temp/scripts
+	cp -R $python_dir/$p $proj_dir/scripts
 done
-cp -R $pydir/$pyrepl.template $proj_temp/scripts/$pyrepl
-cp -R $wardir $proj_temp
 
+cp -R $python_dir/$conf_replace.template $proj_dir/scripts/$conf_replace
+cp -R $war_dir $proj_dir
 
 if [ $setup ]
 then
-	echo "* 1b. setup debian"
-	setupdebian $proj_temp
-	appwrapper
+	echo "* 1a. setup debian in $debian_dir"
+	setupdebian $debian_dir
+	echo "* 1b. setup desktop in $data_dir"
+	setupdesktop $data_dir
 fi
 
 echo "* 2. archive upstream package"
+pwd
+tar -cf $temp_dir/$upstream $proj_dir
+gzip $temp_dir/$upstream
 
-tar -cf $upstream $proj_temp
-gzip $upstream
+#now unpack it all again?
+#rm -R $proj_dir
+#gunzip -k "$temp_dir/$upstream.gz"
+#tar -C / -xf $temp_dir/$upstream
 
-#mkdir "$project"_temp
-#cd "$project"_temp
-#tar -xvf ../$upstream
 
-cd $proj_temp
+cd $proj_dir
+
 echo "* 3. build debian package"
-debuild -us -uc
+dpkg-buildpackage -b
+#debuild -us -uc
 echo "* 4. tidy up"
